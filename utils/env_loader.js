9d67738ebfa1bfa45a958e05fec0b94b94dcc3e5
Pumelela-Banca <pumelelabanca@gmail.com>
2024-03-12 00:42:04 +0200

    changed app controllers

--- utils/env_loader.js
+++ utils/env_loader.js
@@ -1,8 +1,6 @@
 import { existsSync, readFileSync } from 'fs';
 
-/**
- * Loads the appropriate environment variables for an event.
- */
+
 const envLoader = () => {
   const env = process.env.npm_lifecycle_event || 'dev';
   const path = env.includes('test') || env.includes('cover') ? '.env.test' : '.env';
--- utils/mailer.js
+++ utils/mailer.js
@@ -14,12 +14,7 @@ const TOKEN_PATH = 'token.json';
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
+
 async function getNewToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
@@ -48,12 +43,7 @@ async function getNewToken(oAuth2Client, callback) {
   });
 }
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
+
 async function authorize(credentials, callback) {
   const clientSecret = credentials.web.client_secret;
   const clientId = credentials.web.client_id;
@@ -73,11 +63,7 @@ async function authorize(credentials, callback) {
   console.log('Client authorization done');
 }
 
-/**
- * Delivers a mail through the user's account.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- * @param {gmailV1.Schema$Message} mail The message to send.
- */
+
 function sendMailService(auth, mail) {
   const gmail = google.gmail({ version: 'v1', auth });
 
@@ -93,9 +79,6 @@ function sendMailService(auth, mail) {
   });
 }
 
-/**
- * Contains routines for mail delivery with GMail.
- */
 export default class Mailer {
   static checkAuth() {
     readFileAsync('credentials.json')
