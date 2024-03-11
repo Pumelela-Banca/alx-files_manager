4672bb69c150ad8e4e2f92e8653fc7ece9031428
Pumelela-Banca <pumelelabanca@gmail.com>
2024-03-11 17:52:10 +0200

    exprts class

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,7 +2,7 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export default class AppController {
+class AppController {
   static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
@@ -17,3 +17,5 @@ export default class AppController {
       });
   }
 }
+
+export default AppController;
